4a9b9302c65fe55fb88b8333ee723d024683b3fe
AvirutRut <60030112@kmitl.ac.th>
2018-09-14 15:16:19 +0700

    string name "Hello"

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -5,18 +5,26 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            try
-            { 
-                Console.Write("Please enter value 1 :");
-                int val1 = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Please enter value 2 :");
-                int val2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Answer = " + (val1 + val2));
-            }
-        catch (Exception e)
-            {
-                Console.WriteLine("Error : " +e.ToString());
-            }
+            string name = "Hello";
+            Console.WriteLine(String.Format("{0} there. I said {0}! {0}???:", name));
+            Console.WriteLine("{2:d} {0:d} {1:d}", 1, 2, 3);
+            Console.WriteLine("Hello " + "World");
+            Console.WriteLine("Here comes a slash \\");
+            Console.WriteLine("|{0, 10}|", 999);
+            Console.WriteLine("|{0,-10}|", 000);
+            Console.WriteLine("The value: {0}.", 500);
+            Console.WriteLine("The value: {0:C}.", 500);
+            Console.WriteLine("{0,-10:F4}", 12.3456789);
+            Console.WriteLine("{0,-10:C}", 12.3456789);
+            Console.WriteLine("{0,-10:E3}", 12.3456789);
+            Console.WriteLine("{0,-10:x}", 65535);  // (x = lower case)
+            Console.WriteLine("{0,-10:X}", 65535);  // (X = upper case)
+            int i;
+            Console.WriteLine("Value\tSquared\tCubed");
+            for (i = 1; i < 10; i++)
+                Console.WriteLine("{0}\t{1}\t{2}", i, i * i, i * i * i);
+            Console.WriteLine("{0:#.###}.", 1234.56789);
+
         }
     }
 }
